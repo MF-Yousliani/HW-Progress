5325341837fa98034864b0628247fd52481acf4b
MF-Yousliani <77944615+MF-Yousliani@users.noreply.github.com>
2021-01-29 22:59:47 -0500

    Add files via upload

---
+++ Game-1001-Lab02-MohammadFaridYousliani.cpp
@@ -0,0 +1,35 @@
+// Game-1001-Lab02-MohammadFaridYousliani.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    float number1;
+    float number2;
+    float number3;
+
+    cout << "Please Enter number1: \n";
+    cin >> number1;
+
+    cout << "Please Enter number2: \n";
+    cin >> number2;
+
+    cout << "Please Enter number3: \n";
+    cin >> number3;
+    
+
+    cout << "Your Average is: " << (number1 + number2 + number3) /3 ;
+
+
+
+
+
+
+}
+
+
+
+
